0f5d05eb7e22ecc998a8b5fa0b9ca5f648f45379
KIller_Project <81072191+killerprojecte@users.noreply.github.com>
2022-04-13 15:58:10 +0800

    1.1.0-SNAPSHOT V2

--- src/main/java/flyproject/flybuff/utils/ConfigUpdater.java
+++ src/main/java/flyproject/flybuff/utils/ConfigUpdater.java
@@ -1,22 +1,35 @@
 package flyproject.flybuff.utils;
 
 import flyproject.flybuff.FlyBuff;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ConfigUpdater {
     public static void update(){
-        if (FlyBuff.config.get("config-version")==null){
-            System.out.println("[配置更新器] 检测到正在使用1.0.1或之前的版本 开始更新");
-            FlyBuff.config.set("remove",true);
-            FlyBuff.config.set("title","&8[&bBUFF移除面板&8]");
-            FlyBuff.config.set("removehelp","&e - 点击移除BUFF");
-            FlyBuff.config.set("nospace","&c背包空间不足 无法移除BUFF");
-            FlyBuff.config.set("backpage","&f上一页");
-            FlyBuff.config.set("nextpage","&f下一页");
-            FlyBuff.config.set("config-version","1.1.0");
-            FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
+        FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
+        FileConfiguration config = YamlConfiguration.loadConfiguration(new File(buff.getDataFolder() + "/config.yml"));
+        if (config.get("config-version")==null){
+            System.out.println(Color.color("&a[配置更新器] 检测到正在使用1.0.1或之前的版本 开始更新"));
+            config.set("remove",true);
+            config.set("title","&8[&bBUFF移除面板&8]");
+            config.set("removehelp","&e - 点击移除BUFF");
+            config.set("nospace","&c背包空间不足 无法移除BUFF");
+            config.set("backpage","&f上一页");
+            config.set("nextpage","&f下一页");
+            config.set("config-version","1.1.0");
+            config.options().header("由于配置需要更新 注释已被消除 请查看MCBBS上的配置文件栏查看格式");
+            try {
+                config.save(new File(buff.getDataFolder() + "/config.yml"));
+            } catch (IOException e) {
+                System.err.println(Color.color("&c[配置更新器] 保存失败"));
+                return;
+            }
             buff.reloadConfig();
             FlyBuff.config = buff.getConfig();
-            System.out.println("[配置更新器] 更新完成！");
+            System.out.println(Color.color("[配置更新器] 更新完成！"));
         }
     }
 }
