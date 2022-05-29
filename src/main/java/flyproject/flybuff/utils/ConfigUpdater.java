aec23a2e48729082cac22059346d7cbc6e34062c
AnswerForMystery <jim20071128@163.com>
2022-05-29 18:47:49 +0800

    1.2.0 - BETA
    ✔ Particle Support
    ✔ NBT Support (Support Java 8)
    ✔ GUI NBT Support

--- src/main/java/flyproject/flybuff/utils/ConfigUpdater.java
+++ src/main/java/flyproject/flybuff/utils/ConfigUpdater.java
@@ -12,7 +12,7 @@ public class ConfigUpdater {
     public static void update() {
         FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
         FileConfiguration config = YamlConfiguration.loadConfiguration(new File(buff.getDataFolder() + "/config.yml"));
-        if (config.get("config-version") != "1.2.0") {
+        if (config.get("config-version").toString().contains("1.2.0")) {
             System.err.println("[FlyBuff-Updater] 由于1.2.0变更过大 将不提供配置更新 请手动修改更新");
             Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin("FlyBuff"));
         }
