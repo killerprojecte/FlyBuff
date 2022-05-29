4ec7a9c8909fdbeac55d572cc6ff6685ca5bc7a2
AnswerForMystery <jim20071128@163.com>
2022-05-29 11:13:30 +0800

    1.2.0 - ALPHA
    ✔ Particle Support
    ✔ NBT Support (Support Java 8)
    🟠 GUI NBT Support - Pending

--- src/main/java/flyproject/flybuff/FlyBuff.java
+++ src/main/java/flyproject/flybuff/FlyBuff.java
@@ -57,60 +57,79 @@ public final class FlyBuff extends JavaPlugin {
         switch (version){
             case "v1_8_R1": {
                 nms = new NMS_1_8_R1();
+                break;
             }
             case "v1_8_R2": {
                 nms = new NMS_1_8_R2();
+                break;
             }
             case "v1_8_R3": {
                 nms = new NMS_1_8_R3();
+                break;
             }
             case "v1_9_R1": {
                 nms = new NMS_1_9_R1();
+                break;
             }
             case "v1_9_R2": {
                 nms = new NMS_1_9_R2();
+                break;
             }
             case "v1_10_R1": {
                 nms = new NMS_1_10_R1();
+                break;
             }
             case "v1_11_R1": {
                 nms = new NMS_1_11_R1();
+                break;
             }
             case "v1_12_R1": {
                 nms = new NMS_1_12_R1();
+                break;
             }
             case "v1_13_R1": {
                 nms = new NMS_1_13_R1();
+                break;
             }
             case "v1_13_R2": {
                 nms = new NMS_1_13_R2();
+                break;
             }
             case "v1_14_R1": {
                 nms = new NMS_1_14_R1();
+                break;
             }
             case "v1_15_R1": {
                 nms = new NMS_1_15_R1();
+                break;
             }
             case "v1_16_R1": {
                 nms = new NMS_1_16_R1();
+                break;
             }
             case "v1_16_R2": {
                 nms = new NMS_1_16_R2();
+                break;
             }
             case "v1_16_R3": {
                 nms = new NMS_1_16_R3();
+                break;
             }
             case "v1_17_R1": {
                 nms = new NMS_1_17_R1();
+                break;
             }
             case "v1_18_R1": {
                 nms = new NMS_1_18_R1();
+                break;
             }
             case "v1_18_R2": {
                 nms = new NMS_1_18_R2();
+                break;
             }
             default: {
                 logger.error("[FlyBuff] 无法找到对应服务器版本的NMS映射组件 请联系作者!");
+                break;
             }
         }
     }
