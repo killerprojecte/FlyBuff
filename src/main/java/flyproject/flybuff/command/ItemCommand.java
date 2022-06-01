457f39aa3b3aef076c70fa863bd31e09a67a82fd
KIller_Project <81072191+killerprojecte@users.noreply.github.com>
2022-06-01 16:42:46 +0800

    1.2.0-SNAPSHOT
    ItemCommand Support NBT Gem
    Build Script -> Using FastMCMirror Repo Cloud Download Dependencies
    FlyBuff Made by FlyProject with ❤

--- src/main/java/flyproject/flybuff/command/ItemCommand.java
+++ src/main/java/flyproject/flybuff/command/ItemCommand.java
@@ -19,23 +19,44 @@ public class ItemCommand implements CommandExecutor {
             sender.sendMessage(Color.color("&c该命令需要玩家执行"));
         }
         Player p = (Player) sender;
-        if (args.length != 1) {
+        if (args.length != 2) {
             sender.sendMessage(Color.color("&c参数错误"));
+            sender.sendMessage(Color.color("&c/buffitem <nbt/lore> <id>"));
+            return false;
         }
         if (p.getItemInHand() == null || p.getItemInHand().getType().equals(Material.AIR)) {
             sender.sendMessage(Color.color("&c手上物品不能为空"));
+            return true;
         }
-        FlyBuff.item.set("gems." + args[0] + ".mode", "stack");
-        FlyBuff.item.set("gems." + args[0] + ".itemstack", p.getItemInHand());
-        FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
-        try {
-            FlyBuff.item.save(new File(buff.getDataFolder() + "/items.yml"));
-        } catch (IOException e) {
-            sender.sendMessage(Color.color("&c保存文件遇到问题 请检查控制台"));
-            e.printStackTrace();
+        if (args[0].equals("lore")){
+            FlyBuff.item.set("gems." + args[1] + ".mode", "stack");
+            FlyBuff.item.set("gems." + args[1] + ".itemstack", p.getItemInHand());
+            FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
+            try {
+                FlyBuff.item.save(new File(buff.getDataFolder() + "/items.yml"));
+            } catch (IOException e) {
+                sender.sendMessage(Color.color("&c保存文件遇到问题 请检查控制台"));
+                e.printStackTrace();
+            }
+            FlyBuff.item = YamlConfiguration.loadConfiguration(new File(buff.getDataFolder() + "/items.yml"));
+            sender.sendMessage(Color.color("&a保存成功！ItemStack数据已储存至路径\"nbtgem." + args[1] + "\""));
+        } else if (args[0].equals("nbt")){
+            FlyBuff.item.set("nbtgem." + args[1] + ".mode", "stack");
+            FlyBuff.item.set("nbtgem." + args[1] + ".itemstack", p.getItemInHand());
+            FlyBuff buff = FlyBuff.getPlugin(FlyBuff.class);
+            try {
+                FlyBuff.item.save(new File(buff.getDataFolder() + "/items.yml"));
+            } catch (IOException e) {
+                sender.sendMessage(Color.color("&c保存文件遇到问题 请检查控制台"));
+                e.printStackTrace();
+            }
+            FlyBuff.item = YamlConfiguration.loadConfiguration(new File(buff.getDataFolder() + "/items.yml"));
+            sender.sendMessage(Color.color("&a保存成功！ItemStack数据已储存至路径\"nbtgem." + args[1] + "\""));
+        } else {
+            sender.sendMessage(Color.color("&c参数错误"));
+            sender.sendMessage(Color.color("&c/buffitem <nbt/lore> <id>"));
+            return false;
         }
-        FlyBuff.item = YamlConfiguration.loadConfiguration(new File(buff.getDataFolder() + "/items.yml"));
-        sender.sendMessage(Color.color("&a保存成功！ItemStack数据已储存至路径\"gems." + args[0] + "\""));
         return true;
     }
 }
